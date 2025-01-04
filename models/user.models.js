e0bd38639ab809d96d71661323c715a3c3ec0081
suryamazumder <suryakantamazumder9@gmail.com>
2025-01-04 22:02:25 +0530

    mongoose usermethods ,aggregate pipeline use (access token,refresh token)

--- models/user.models.js
+++ models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchemea = new mongoose.Schema(
   {
     username: {
@@ -45,5 +46,41 @@ const userSchemea = new mongoose.Schema(
     timestamps: true,
   }
 );
+//implention of pre hook
+userSchemea.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+//custom method design moongose
+userSchemea.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//access token generation by jwt
+userSchemea.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+//refresh token generation
+userSchemea.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchemea);
